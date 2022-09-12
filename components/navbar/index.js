696afa0c40e25e193e2f19f4c1a2a22269572bbf
Alexander <sashafromlibertalia@gmail.com>
2022-09-12 22:33:01 +0300

    style: added variables

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -6,6 +6,7 @@ class Navbar extends HTMLElement {
 <nav class="navigation">
   <a data-link="main" href="index.html" class="navigation__item">Главная</a>
   <a data-link="feedback" href="feedback.html" class="navigation__item">Отзывы о моей работе</a>
+  <a data-link="schedule" href="schedule.html" class="navigation__item">Создать расписание</a>
 </nav>
 `
   }
--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ const paths = Object.freeze({
     file: 'itmo-web/feedback.html',
     link: 'feedback'
   },
+  SCHEDULE: {
+    file: 'itmo-web/schedule.html',
+    link: 'schedule'
+  }
 });
 
 (() => {
@@ -27,6 +31,8 @@ const paths = Object.freeze({
       case document.location.pathname.includes(paths.FEEDBACK.file):
         [...navigationLinks].find(link => link.dataset.link === paths.FEEDBACK.link).classList.add('navigation__item_active');
         break;
+      case document.location.pathname.includes(paths.SCHEDULE.file):
+        [...navigationLinks].find(link => link.dataset.link === paths.SCHEDULE.link).classList.add('navigation__item_active');
     }
   })
 })();
