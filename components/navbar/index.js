66a91c818ba7e3cda093a4f39199130a8f1b8dcf
Alexander <sashafromlibertalia@gmail.com>
2022-09-14 20:30:08 +0300

    fix: fixed layout on mobile devices

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -5,8 +5,8 @@ class Navbar extends HTMLElement {
     this.innerHTML = `
 <nav class="navigation">
   <a data-link="main" href="index.html" class="navigation__item">Главная</a>
-  <a data-link="feedback" href="feedback.html" class="navigation__item">Отзывы о моей работе</a>
-  <a data-link="schedule" href="schedule.html" class="navigation__item">Создать расписание</a>
+  <a data-link="feedback" href="feedback.html" class="navigation__item">Отзывы</a>
+  <a data-link="schedule" href="schedule.html" class="navigation__item">Расписание</a>
 </nav>
 `
   }
